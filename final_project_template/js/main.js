4d3c14313f1cbc54c3c34578da52c3426546a901
zails23 <zails@yahoo.com>
2017-08-14 21:32:21 -0700

    Final Project Progress So Far

--- final_project_template/js/main.js
+++ final_project_template/js/main.js
@@ -1,15 +1,23 @@
-// When a user scrolls to the section My work (.work)
-  // Slowly fadeIn the class "row" (.row)
-  // Else Hide .work
 
-// When a user clicks on My Work in the nav,
-  //  Jump to My Work section (.work)
-  //  Slowly fadeIn the class "row" (.row)
-  //  Else hide .work
+$(window).on('scroll', function () {
+  var distanceScrolled = $(window).scrollTop();
+  console.log(distanceScrolled);
 
-// When a user scrolls to the section favorite things (.favethings)
-  //  Slowly fadeIn the section (.favethings)
-  //  Else hide .favethings
+  // $('.fave-things').not(':offscreen').addClass('faded-in');
+  // $('.projects').not(':offscreen').addClass('faded-in');
+  //
+  if ($('.fave-things') )
+  // if ($(document).scrollTop() >= 650) {
+  //   $('.projects').addClass('faded-in');
+  // }
+  // if ($(document).scrollTop() >= 1) {
+  //   console.log('true');
+  //   $('.fave-things').addClass('faded-in');
+  //
+  // }
+});
+
+// when target offset top = (value offset of the window) => then trigger fade in. 
 
 // When a user scrolls to the contact section (.contact)
-  //  Slowly fadeIn the section .contact 
+//  Slowly fadeIn the section .contact
